5efcc0a9731e88e30555259958649d0b40e61c25
tianminghui <tianmh2013@163.com>
2024-11-23 19:08:32 +0800

    fix

--- tests/test.py
+++ tests/test.py
@@ -1,32 +1,69 @@
+from datetime import timedelta
 import os
 import sys
 
-from pytokenx import TokenManager, FileTokenStorage, token_validator, flask_token_validator
+from pytokenx import TokenManager, FileTokenStorage, token_validator, flask_token_validator, TokenStorage
 
 # 使用文件存储
 token_manager = TokenManager(FileTokenStorage("tokens.json"))
 # sqlite存储
 # token_manager = TokenManager(SQLAlchemyTokenStorage(connection_string="sqlite:///test.db"))
-token = token_manager.generate_token(user_id="test_user", extra_data = {"name": "test_name"}) # 生成token
-print(token)  # MieZqFUchiasygXW
-token_data = token_manager.validate_token(token) # 验证token
-if token_data:
-    print(token_data)  # {'token': 'MieZqFUchiasygXW', 'token_type': 'default', 'user_id': 'test_user', 'extra_data': {'name': 'test_name'}, 'created_at': '2024-11-07T14:12:17.389325', 'expires_at': None, 'deleted_at': None, 'is_active': True}
-else:
-    print("token 无效")
+# 自定义存储
+# class CustomTokenStorage(TokenStorage):
+#     xxxx
 
+# 生成token
+token1 = token_manager.generate_token(user_id="test_user", expiry= timedelta(days=1)) 
+print("generate token1 with expiry ",token1)  # MieZqFUchiasygXW
+# 验证token
+token_data = token_manager.validate_token(token1) 
+print("validate token1  pass", token_data)
 
+# 生成带quota的token
+token2 = token_manager.generate_token(expiry= timedelta(days=1), quota=10)
+print("generate token2 with quota",token2)
+# 验证带quota的token, 仅验证，不扣除
+token_data = token_manager.validate_token(token2, cost_quota=2 , deduct_quota=False)
+print("validate token2  pass", token_data)
+# 扣除quota
+token_manager.deduct_quota(token2, 2)
+# 获取token数据
+token_data = token_manager.get_token_data(token2)
+print("get token2 data",token_data)
 # 使用装饰器
 @token_validator(token_manager)
 def my_function(token):
     print(token)
-    print(token_manager.get_current_token_data())  # {'token': 'MieZqFUchiasygXW', 'token_type': 'default', 'user_id': 'test_user', 'extra_data': {'name': 'test_name'}, 'created_at': '2024-11-07T14:12:17.389325', 'expires_at': None, 'deleted_at': None, 'is_active': True}
+    print(token_manager.get_current_token_data())  # 从当前线程中获取当前token数据 
 
-my_function(token=token)
+my_function(token=token2)
 
-token_manager.delete_token(token) # 删除token
+
+# # flask装饰器
+# @app.route("/get_code", methods=["POST"])
+# @flask_token_validator(token_manager)
+# def get_code():
+#     pass 
+
+# 通用装饰器
+# 1.定义获取token的方法
+def extract_token_func(*args, **kwargs) -> str:
+    return args[0]
+
+# 使用装饰器
+@token_validator(token_manager, extract_token_func=extract_token_func)
+def my_function_custom(token):
+    print(token)
+
+my_function_custom(token2)
+
+
+# 删除token
+token_manager.delete_token(token2) 
 
 token_manager.storage.close()
+
+
 
 if os.path.exists("tokens.json"):
     os.remove("tokens.json")
